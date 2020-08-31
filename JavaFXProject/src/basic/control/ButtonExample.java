62ee65ade0032bf64711666c6749f50622b17580
Oivevri <oivevri@gmail.com>
2020-08-31 14:08:28 +0900

    200831

--- JavaFXProject/src/basic/control/ButtonExample.java
+++ JavaFXProject/src/basic/control/ButtonExample.java
@@ -1,5 +1,26 @@
 package basic.control;
+// UI : Button.fxml
+// Controller : ButtonController.java
 
-public class ButtonExample {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
+public class ButtonExample extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		BorderPane bt
+		= FXMLLoader.load(getClass().getResource("Button.fxml"));
+	
+	Scene sc = new Scene(bt);
+	primaryStage.setScene(sc);
+	primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
 }
